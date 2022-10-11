0448015a6c063bc88118b2c298c39c42a60a3e24
凤之兮原 <1577310596@qq.com>
2022-10-11 21:25:58 +0800

    feat: add rand dependency

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
 use std::io;
+use rand::Rng;
 fn main() {
-    println!("Guess the number:")
+    println!("Guess the number:");
 
-    let select_number = rand::
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
+    println!("The secret_number is: { secret_number }");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Fail to read line");
+
+    println!("Oh, you get it");
 }
